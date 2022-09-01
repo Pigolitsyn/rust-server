e4a232dc527d9afdd6924b2bb51cf5f291b104b4
pigolitsyn <semen23112002@mail.ru>
2022-09-02 00:08:45 +0300

    fix

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,5 @@ fn rocket() -> _ {
     rocket::build().manage(Cookies)
         .attach(controllers::post_controller::post_controller())
         .attach(controllers::user_controller::user_controller())
-        .attach(AdHoc::on_request("Put Rewriter", |req, _| Box::pin(async move {
-            req.cookies();
-            State
-        })))
     .mount("/", FileServer::new(relative!("static"), Options::Index).rank(30))
 }
